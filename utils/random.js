eab269ca9482e13cea3debce9a76c135d8583836
Ailuj <92523063+AilujTU@users.noreply.github.com>
2026-05-12 23:25:19 +0200

    1 implementierung eines wer fängt an befehls (#3)
    * modified intents access and command deployment
    
    * added base functionality & shuffle algo
    
    * added some variation is messaging for fun & updated visualisation
    
    * fixed bug allowing adding bot as last speaker

--- utils/random.js
+++ utils/random.js
@@ -4,6 +4,23 @@ function rollDice(max) {
     return crypto.randomInt(1,max+1);
 }
 
+function randomInt(min,max) {
+    return crypto.randomInt(min,max+1);
+}
+
+// fisher-yates-shuffle
+function shuffle(array) {
+    for (let i = array.length -1; i > 0; i--) {
+        let j = crypto.randomInt(i,array.length);
+
+        [array[i],array[j]] = [array[j],array[i]];
+    }
+
+    return array;
+}
+
 module.exports = {
     rollDice,
+    shuffle,
+    randomInt,
 };

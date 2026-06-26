c0b3f744146925c2414b7b3830141bf945ff208c
Ailuj <92523063+AilujTU@users.noreply.github.com>
2026-06-27 00:47:22 +0200

    17 fix bot not joining channel if online later (#18)

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,11 @@
 const { Events } = require('discord.js');
+const {restorePersistentTrackers} = require('../utils');
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		await restorePersistentTrackers(client); // TODO: delete if fully finished w/ voice handler
 	},
 };

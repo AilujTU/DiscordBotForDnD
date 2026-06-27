ca0a85b17eed0522b5c69e5708b2327eded55d0e
Ailuj <92523063+AilujTU@users.noreply.github.com>
2026-06-27 13:50:05 +0200

    added necessary tables to db

--- db/migrate.js
+++ db/migrate.js
@@ -39,7 +39,50 @@ async function migrate() {
         });
     }
 
-    console.log('Database successfully migrated.')
+    const hasTrackedMemberTable = await db.schema.hasTable('trackedMember');
+
+    if (!hasTrackedMemberTable) {
+        await db.schema.createTable('trackedMember', table => {
+            table.increments('id').primary();
+            table.string('member_id').notNullable();
+            table.string('channel_id').notNullable();
+            table.double('last_session_percentage');
+            table.double('all_time_percentage');
+        });
+    }
+
+    const hasSpeechPlacementStatTable = await db.schema.hasTable('speechPlacementStat');
+
+    if (!hasSpeechPlacementStatTable) {
+        await db.schema.createTable('speechPlacementStat', table => {
+            table.increments('id').primary();
+            table.integer('tracked_member_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('trackedMember')
+                .onDelete('CASCADE');
+            table.integer('position').notNullable();
+            table.integer('count').notNullable().defaultTo(0);
+        });
+    }
+
+    const hasSpeechYearlyStatTable = await db.schema.hasTable('speechYearlyStat');
+
+    if (!hasSpeechPlacementStatTable) {
+        await db.schema.createTable('speechYearlyStat', table => {
+            table.increments('id').primary();
+            table.integer('trackedMember_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('trackedMember');
+            table.string('month');
+            table.double('percentage').notNullable().defaultTo(0);
+        });
+    }
+
+    console.log('Database successfully migrated.');
     await db.destroy();
 }
 
